edeec25a0ff9039b6e5b8a0c5ba3ba6dfe08b2b5
originality666 <18561815350@163.com>
2025-10-20 22:46:13 +0800

    add lesson12

---
+++ lesson12/build/CMakeFiles/global_memory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/global_memory.dir/global_memory.cu.o"
+  "global_memory"
+  "global_memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/global_memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
