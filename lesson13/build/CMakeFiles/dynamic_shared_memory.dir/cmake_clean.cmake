ffb3678b22f86efd0844c364a12a8895cd7f675f
originality666 <18561815350@163.com>
2025-10-20 23:34:14 +0800

    add lesson13

---
+++ lesson13/build/CMakeFiles/dynamic_shared_memory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamic_shared_memory.dir/dynamic_shared_memory.cu.o"
+  "dynamic_shared_memory"
+  "dynamic_shared_memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/dynamic_shared_memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lesson13/build/CMakeFiles/static_shared_memory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/static_shared_memory.dir/static_shared_memory.cu.o"
+  "static_shared_memory"
+  "static_shared_memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/static_shared_memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
